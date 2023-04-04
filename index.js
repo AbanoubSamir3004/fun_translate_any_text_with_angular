ae5dd9e6e3ce4e06a8e7d34b3b73e46fabc9cb10
Abanoub Samir <abanoubsamir3004@gmail.com>
2023-04-04 16:53:58 +0200

    start

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let x = `Years old`;
+let x = `Ye ar s old`;
 let n = "ROOMS";
 
 function translateFun(x, n) {
-  let y = x.toLocaleUpperCase().replaceAll(/[,؟ ?.’0123465798-]/g, "");
+  let y = x.toLocaleUpperCase().replace(/[,؟ ?.’0123465798-]/g, "");
 
   let p = `"${y}":"${x}",`;
   let t = `{{'${n}.${y}' | translate}}`;
